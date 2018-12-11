8ce9fa844cbdbd4f3b8b217f6bde64b53e5b1c11
KGB33 <33pigbass@gmail.com>
2018-12-11 07:37:59 -0600

    Added p025 and p026

---
+++ p025 - 1000-digit Fibonacci Number.py
@@ -0,0 +1,50 @@
+"""
+1000-digit Fibonacci number
+Problem 25
+The Fibonacci sequence is defined by the recurrence relation:
+
+Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+Hence the first 12 terms will be:
+
+        F1 = 1
+        F2 = 1
+        F3 = 2
+        F4 = 3
+        F5 = 5
+        F6 = 8
+        F7 = 13
+        F8 = 21
+        F9 = 34
+        F10 = 55
+        F11 = 89
+        F12 = 144
+
+The 12th term, F12, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+"""
+from PrimeTools import timer
+
+
+def p025():
+    index = 2
+    fn_1 = 1
+    fn = 1
+    while len(str(fn)) != 1000:
+        hold = fn
+        fn = next_fib(fn, fn_1)
+        fn_1 = hold
+        index += 1
+    print("Solution: {}".format(index))
+
+
+def next_fib(fn_1, fn_2):
+    return fn_1 + fn_2
+
+
+@timer()
+def main():
+    p025()
+
+
+main()
