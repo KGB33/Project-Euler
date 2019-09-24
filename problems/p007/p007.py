bcc2e611669b500f9c4a023a96c779dfa67c3ab7
KGB33 <33pigbass@gmail.com>
2019-09-23 20:58:56 -0500

    optimizations

--- problems/p007/p007.py
+++ problems/p007/p007.py
@@ -1,18 +1,21 @@
 import time
-from PrimeTools import division_test
+from PrimeTools import sieve_of_eratosthenes
+from math import log
 
 
 def p007():
-    i = 2
-    p = 3
-    while i != 10001:
-        p += 2
-        if division_test(p):
-            i += 1
-    return p
+    guess_num = 10001
+    while True:
+        p_guess = int(guess_num * log(guess_num, 10))
+        p = sieve_of_eratosthenes(p_guess)
+        primes = [x for x in p if p[x]]
+        if len(primes) > 10000:
+            return primes[10000]
+        else:
+            guess_num *= 1.5
 
 
 if __name__ == "__main__":
-    start_time = time.clock()
+    start_time = time.perf_counter()
     print(p007())
-    print(time.clock() - start_time)
+    print(time.perf_counter() - start_time)
